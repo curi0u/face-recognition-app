c4378ffe9b619a93c13ce66d9e8bbccfc3ec7311
curi0u <123840989+curi0u@users.noreply.github.com>
2024-06-12 22:33:43 -0400

    Added input functionality and made initial requests to Clarifai API

--- src/App.js
+++ src/App.js
@@ -3,10 +3,65 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
 import './App.css';
 
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = '943a30a84f154737b55dde05d2ab2667';
+  const USER_ID = 'curi0u';       
+  const APP_ID = 'my-first-application-u7qo9';   
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  return requestOptions
+}
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +69,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     )
   }
