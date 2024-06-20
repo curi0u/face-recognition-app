b82ccec93ac24ede741b5ac2c00a457c3c64c09d
curi0u <123840989+curi0u@users.noreply.github.com>
2024-06-19 20:42:27 -0400

    Code cleanup

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const knex = require('knex');
 
+const signin = require('./controllers/signin')
+const register = require('./controllers/register')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors())
 
-const saltRounds = 10;
 const db = knex({
   client: 'pg',
   connection: {
@@ -24,78 +28,13 @@ app.get('/', (req, res) => {
   res.send('success');
 })
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-      .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) {
-          return db.select('*').from('users')
-            .where('email', '=', req.body.email)
-            .then(user => {
-              res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        } else {
-          res.status(400).json('wrong credentials')
-        }
-      })
-      .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, bcrypt, db) })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password, saltRounds);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0].email,
-          name: name,
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0]);
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, bcrypt, db) })
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({
-    id: id
-  })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0].entries)
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(3000, () => {
   console.log('app is listening on port 3000');
--- src/App.js
+++ src/App.js
@@ -42,6 +42,22 @@ const returnClarifaiRequestOptions = (imageUrl) => {
 
   return requestOptions
 }
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
@@ -110,6 +126,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, {entries: count}))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(result))
 
@@ -119,7 +136,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
